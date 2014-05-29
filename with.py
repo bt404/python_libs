25b51d9cfaf8cd05ca7586a957624d34ca2e7219
knd2 <gjz.knd2@gmail.com>
2014-05-29 13:23:16 +0800

    fix

--- with.py
+++ with.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+# must return an object in __enter__ when use
+# with statement. order of invocation :
+# __enter__ --> with block --> __exit__
 class Sample(object):
     def __enter__(self):
         print "In __enter__"
+        return self
 
     def __exit__(self, type, value, trace):
         print "In __exit__"
@@ -10,16 +14,13 @@ class Sample(object):
         print "value: ", value
         print "trace: ", trace
 
-    def error_open(self):
-        file = open('notexists')
-        content = file.read()
-
     def bad_division(self):
-        ret = 2/0
+        print "during exec"
+        ret = 2/0   # raise exception
 
 
 if __name__ == "__main__":
     with Sample() as sample:
         sample.bad_division()
-    with open('not exists') as sample:
-        sample.read()
+    #with open('not exists') as sample:  # with don't try exception before going into block
+    #    sample.read()
