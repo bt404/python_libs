576224b5c04e423f9a4b2cc1b89d28da3764cdd8
knd2 <gjz.knd2@gmail.com>
2014-09-22 22:55:45 +0800

    fix

--- singleton.py
+++ singleton.py
@@ -5,7 +5,7 @@ class Singleton(object):
     
     def __new__(cls, *args, **kw):
         if not hasattr(cls, "_instance"):
-            if(lock.acquire()):
+            lock.acquire()
                 if not hasattr(cls, "_instance"):
                     orig = super(Singleton, cls)
                     cls._instance = orig.__new__(cls, *args, **kw)
